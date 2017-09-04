7e86629422e04a11de8ac2ae469084abd1af25d6
tb7459 <31620023+tb7459@users.noreply.github.com>
2017-09-04 14:00:50 -0500

    updated reformatted code
    reformatted code using the PEP8 online tool

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,36 +1,41 @@
-#import here
+# import here
 import fresh_tomatoes
 import media
 
-
-#create instances of the Movie object below for avatar,prometheus
-#wonder woman, deadpool, princess bride, and warcraft
+# create instances of the Movie object below for avatar,prometheus
+# wonder woman, deadpool, princess bride, and warcraft
+avatarPic = "https://upload.wikimedia.org/wikipedia/"
+avatarPic = avatarPic + "en/b/b0/Avatar-Teaser-Poster.jpg"
+promPic = "https://20ui41tp7v127j03rcnp97oh-wpengine.netdna-ssl.com/"
+promPic = promPic + "wp-content/uploads/2012/11/prometheusbg3.jpg"
+wwPic = "https://cdn.traileraddict.com/content/warner-bros-pictures/"
+wwPic = wwPic + "wonder-woman-2017-10.jpg"
+dpPic = "https://pre11.deviantart.net/1f9a/th/pre/f/2015/"
+dpPic = dpPic + "135/3/3/deadpool_movie_poster_by_chronoxiong-d8tfamw.png"
+pbPic = "http://4.bp.blogspot.com/-dT5wdvnKZ1s/Te1XqUzvPHI/AAAAAAAAAPE/"
+pbPic = pbPic + "3zluGjAUwlA/s1600/princess-bride.jpg"
+wcPic = "http://f.ptcdn.info/049/037/000/"
+wcPic = wcPic + "nx8f2tnujPE0mnzOa8U-o.jpg"
 
 avatar = media.Movie("Avatar", "A marine on an alien Planet",
-                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+                     avatarPic,
                      "https://www.youtube.com/watch?v=5PSNL1qE6VY")
-
-
-prom = media.Movie("Prometheus","Archaeologists find their creators",
-                   "https://20ui41tp7v127j03rcnp97oh-wpengine.netdna-ssl.com/wp-content/uploads/2012/11/prometheusbg3.jpg",
+prom = media.Movie("Prometheus", "Archaeologists find their creators",
+                   promPic,
                    "https://www.youtube.com/watch?v=34cEo0VhfGE")
-
-ww = media.Movie("Wonder Woman","Amazonian warrior princess Diana defeats Aries",
-                 "https://cdn.traileraddict.com/content/warner-bros-pictures/wonder-woman-2017-10.jpg",
+ww = media.Movie("Wonder Woman", "Amazon warrior princess Diana defeats Aries",
+                 wwPic,
                  "https://www.youtube.com/watch?v=VSB4wGIdDwo")
-
-dp = media.Movie("Deadpool","A not-so-super superhero battles his creator",
-                 "https://pre11.deviantart.net/1f9a/th/pre/f/2015/135/3/3/deadpool_movie_poster_by_chronoxiong-d8tfamw.png",
+dp = media.Movie("Deadpool", "A not-so-super superhero battles his creator",
+                 dpPic,
                  "https://www.youtube.com/watch?v=ONHBaC-pfsk")
-
-princess_bride = media.Movie("The Princess Bride","As you wish...",
-               "http://4.bp.blogspot.com/-dT5wdvnKZ1s/Te1XqUzvPHI/AAAAAAAAAPE/3zluGjAUwlA/s1600/princess-bride.jpg",
-               "https://www.youtube.com/watch?v=GNvy61LOqY0")
-
-wc = media.Movie("Warcraft","A pc game comes to the theater",
-                 "http://f.ptcdn.info/049/037/000/nx8f2tnujPE0mnzOa8U-o.jpg",
+princess_bride = media.Movie("The Princess Bride", "As you wish...",
+                             pbPic,
+                             "https://www.youtube.com/watch?v=GNvy61LOqY0")
+wc = media.Movie("Warcraft", "A pc game comes to the theater",
+                 wcPic,
                  "https://www.youtube.com/watch?v=RhFMIRuHAL4")
 
-#create an array of movie instances to send to fresh_tomatoes 
-movies = [avatar,prom,ww,dp,princess_bride,wc]
+# create an array of movie instances to send to fresh_tomatoes
+movies = [avatar, prom, ww, dp, princess_bride, wc]
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,26 +1,27 @@
-#import webbrowser to open the trailers
+# import webbrowser to open the trailers
 import webbrowser
 
-#create class Video, which will be the parent class, with title as
-# a variable.
+
 class Video():
-  VALID_RATINGS = ["G","PG","PG-13","R"]
-  def __init__(self,video_title):
-      self.title = video_title
+    # create class Video, which will be the parent class,
+    # with title as a variable.
+    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+
+    def __init__(self, video_title):
+        self.title = video_title
 
 
-#create class Movie, which inherits from Video class
 class Movie(Video):
-  """ This class provides a way to store movie related information"""
-  #initialize movie variables
-  def __init__(self,movie_title, movie_storyline,poster_image,
-               trailer_youtube):
+    # create class Movie, which inherits from Video class
+    """ This class provides a way to store movie related information"""
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
+        # initialize movie variables
+        Video.__init__(self, movie_title)
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
 
-      Video.__init__(self,movie_title)
-      self.storyline = movie_storyline
-      self.poster_image_url = poster_image
-      self.trailer_youtube_url = trailer_youtube
-      
-  #open webbrowser and start trailer
-  def show_trailer(self):
-      webbrowser.open(self.trailer_youtube_url)
+    def show_trailer(self):
+        # open webbrowser and start trailer
+        webbrowser.open(self.trailer_youtube_url)
